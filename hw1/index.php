32295a783197034dd26e8c31c8a8cad231eb7b63
Алексей Чёрный <alex0637555229@gmail.com>
2018-04-27 16:56:48 +0300

    Update index.php

--- hw1/index.php
+++ hw1/index.php
@@ -1,9 +1,5 @@
 <?php
 $age = intval((time()-mktime(0, 0, 0, 9, 3, 1989))/31536000, 10);
-echo $age.'<br>';
-
-
-
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -11,10 +7,8 @@ echo $age.'<br>';
     <meta charset="UTF-8">
     <title>Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
 </head>
 <body>
 Меня зовут Алексей Чёрный, мне <?= $age ?> лет и я бородат.
-
 </body>
 </html>
