e34241bdd86e5083a042496f507867d4652831a8
Алексей Чёрный <alex0637555229@gmail.com>
2018-05-06 13:47:29 +0300

    Add files via upload

--- hw3/models/Posts.php
+++ hw3/models/Posts.php
@@ -2,15 +2,15 @@
 
 class Posts
 {
-    private $feeds = [
+    private static $feeds = [
         'https://tproger.ru/feed/',
         'https://apostrophe.ua/site/allfeed',
     ];
 
-    public function getPosts()
+    public static function getPosts()
     {
-        $feedIndex = rand(0, count($this->feeds)-1);
-        $rss = file_get_contents($this->feeds[$feedIndex]);
+        $feedIndex = rand(0, count(self::$feeds)-1);
+        $rss = file_get_contents(self::$feeds[$feedIndex]);
         $data = new SimpleXMLElement($rss, LIBXML_NOCDATA );
         return $data->channel->item;
     }
